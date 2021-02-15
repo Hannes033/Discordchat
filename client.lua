db9fb8410c6b7389a0dd515bf9f24a410ce52700
Hannes033 <57415875+Hannes033@users.noreply.github.com>
2021-02-15 18:19:02 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,8 @@
+-- NOT EDIT IT
+RegisterCommand('discord', function()
+    TriggerEvent('chat:addMessage',{
+        coler: {255, 255. 0}
+        multiline true,
+        args = {'Zaanstad', 'discord.io/zaanstadroleplaynl'}
+    })
+end) -- command in chat /discord
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+games ['gta5']
+
+author 'Hannes#7470'
+description '/discord'
+version '1.0.0'
+
+client_scripts {
+    "client.lua"
+}
