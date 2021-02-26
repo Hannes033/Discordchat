4afb0d2ffc2ca9b01e797f5e43bd09cd2430b642
Hannes033 <57415875+Hannes033@users.noreply.github.com>
2021-02-26 12:40:09 +0100

    Delete client.lua

--- client.lua
+++
@@ -1,8 +0,0 @@
--- NOT EDIT IT
-RegisterCommand('discord', function()
-    TriggerEvent('chat:addMessage',{
-        coler: {255, 255. 0}
-        multiline true,
-        args = {'SERVER NAME', 'YOUR MASSAGE'}
-    })
-end) -- command in chat /discord
